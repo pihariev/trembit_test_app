591bd2c39df2b1dd7a3b4ed2f6fca0cca3d5be3b
Roman Pihariev <pihariev@gmail.com>
2019-08-03 19:29:52 +0300

    Add movie repository.

---
+++ lib/data/repository/movie_repository.dart
@@ -0,0 +1,36 @@
+import 'package:trembit_test_app/data/mapper/mapper.dart';
+import 'package:trembit_test_app/data/mapper/movie_mapper.dart';
+import 'package:trembit_test_app/data/service/movie_service.dart';
+import 'package:trembit_test_app/model/response/movie_response.dart';
+import 'package:trembit_test_app/model/result.dart';
+import 'package:trembit_test_app/model/ui/movie.dart';
+
+final IMovieRepository movieRepository =
+    MovieRepository._(movieService, MovieMapper());
+
+abstract class IMovieRepository {
+  Future<Result<List<Movie>>> getUpcomingMovies();
+}
+
+class MovieRepository extends IMovieRepository {
+  final IMovieService _movieService;
+  final Mapper<MovieResponse, Movie> _movieMapper;
+
+  MovieRepository._(this._movieService, this._movieMapper);
+
+  @override
+  Future<Result<List<Movie>>> getUpcomingMovies() async {
+    try {
+      final response = await _movieService.getUpcomingMovies();
+      final movies = response.items
+          .map((movieResponse) => _movieMapper.map(movieResponse))
+          .take(10)
+          .toList();
+      final result = Result.success(movies);
+      return Future.value(result);
+    } catch (e) {
+      final result = Result<List<Movie>>.error(e.toString());
+      return Future.value(result);
+    }
+  }
+}
