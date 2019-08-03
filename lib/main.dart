d8697f4abeb16caf6af0bbfc1124c2cdf55bcc80
Roman Pihariev <pihariev@gmail.com>
2019-08-03 19:46:41 +0300

    Add routes.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:trembit_test_app/route.dart' as AppRoute;
 
 void main() => runApp(MyApp());
 
@@ -11,6 +12,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: Scaffold(),
+      initialRoute: AppRoute.Route.ROOT,
     );
   }
 }
---
+++ lib/route.dart
@@ -0,0 +1,8 @@
+class Route {
+  Route._();
+
+  static const ROOT = '/';
+  static const UPCOMING_MOVIES_PAGE = '/upcoming_movies_page';
+  static const MOVIE_DETAILS_PAGE = '/movie_details_page';
+  static const SETTINGS_PAGE = '/settings_page';
+}
